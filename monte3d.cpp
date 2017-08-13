1979fcb5f0dd728b57953cc944d88e1c3f14484a
Shkrwnd <tiwari.willfriends.shikhar@gmail.com>
2017-08-13 23:45:15 +0530

    Added 3D file

--- monte3d.cpp
+++ monte3d.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 #define tab			"\t"
 
-#define N 10
+#define N 2+2+2
 #define NDIM 3
 #define NS pow(N,NDIM)
 #define N2 NS/2
@@ -20,11 +20,8 @@ using namespace std;
 #define NTEMP 6
 #define NMEAS 2+1
 #define INFINITE 10000
-#define HIGH INFINITE
-#define sqlat0 occ
-#define sqlat1 occ
-#define VER N-1
-#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+
+
 
 
 
@@ -41,25 +38,9 @@ long double myrand(long iseed) {
 	return uniformdist();
 }
 
-void printmat(float (*phi)[N ]) {
-	for (int i = 0; i < N ; i++) {
-		for (int j = 0; j < N ; j++) {
-			cout << phi[i][j] << "\t";
-		}
-		cout << endl;
-	}
-}
-void printmat1(int(*occ)[N ]) {
-	for (int i = 0; i < N ; i++) {
-		for (int j = 0; j < N ; j++) {
-			cout << occ[i][j] << "\t";
-		}
-		cout << endl;
-	}
-}
 
 ///  Initializing phi[][]  randomly
-void rsiteen(int n, int iseed,float (*phi)[N][N] ,float w) {
+void rsiteen(int n, int iseed,float phi[][N][N] ,float w) {
 	
 	
 	int i, j, n2, k;
@@ -68,7 +49,7 @@ void rsiteen(int n, int iseed,float (*phi)[N][N] ,float w) {
 		for (j = 1; j < N-1; j++) {
 			for (int k = 1;k< N-1;k++){
 				aa = myrand( iseed);
-
+                //cout << "a" << aa <<endl;
 				if (aa <= 0.5) {
 					phi[i][j][k] = -1 * w;
 				}
@@ -80,12 +61,21 @@ void rsiteen(int n, int iseed,float (*phi)[N][N] ,float w) {
 		}
 	}
 	//printmat(phi);
+    for (int i = 0; i < N; i++) {
+		for (int j = 0; j <N; j++) {
+			for (int k = 0;k < N ;k++ ){
+                //z[i][j] += 10;
+			    cout << phi[i][j][k] << "\t" ;
+            }
+            cout << endl;
+		} 
+	}
 	
 
 }
 
 ///// Initializing occ[][] randomly
-void rocc(int n,int iseed,int (*occ)[N][N]) {
+void rocc(int n,int iseed,int occ[][N][N]) {
 	float a;
 	
 	
@@ -111,76 +101,107 @@ void rocc(int n,int iseed,int (*occ)[N][N]) {
 		}
 		for (int i = 1; i < N-1; i++) {
 			occ[i][N-1][k] = occ[i][1][k];
-			occ[i][0] = occ[i][N-2][k];
+			occ[i][0][k] = occ[i][N-2][k];
 		}
 	}
-	
+	/*for (int i = 0; i < N; i++) {
+		for (int j = 0; j <N; j++) {
+			for (int k = 0;k < N ;k++ ){
+                //z[i][j] += 10;
+			    cout << occ[i][j][k] << endl;
+            }
+		} 
+	}*/
 
 	
 }
 
 
-void hamiltonian(int n, int occ[N][N],float phi[N][N],float &toten ,float &totenst) {
+///  Stores site energy which corresponds to 4 neighbours in    sten1[][]
+void siteEnergy(int n, int occ[N][N][N], float phi[N][N][N], float sten1[][N][N]) {
+	for (int ix = 1; ix < n - 1; ix++) {
+		for (int iy = 1; iy < n - 1; iy++) {
+			for(int iz = 1;iz < n-1;iz++){
+				sten1[ix][iy][iz] = -1 * phi[ix][iy][iz];
+				sten1[ix][iy][iz] = sten1[ix][iy][iz] - (occ[(ix) + 1][iy][iz] + occ[(ix) - 1][iy] [iz]+ occ[ix][(iy) + 1][iz] + occ[ix][(iy) - 1][iz] + occ[ix][iy][iz+1]+ occ[ix][iy][iz-1]);
+			}
+			
+		}
+	}
+	
+	
+	
+}
+
+
+void printz(int z[][N][N]) {
+	/*for (int i = 0; i < N; i++) {
+		for (int j = 0; j <N; j++) {
+			for (int k = 0;k < N ;k++ ){
+                //z[i][j] += 10;
+			    z[i][j][k] = i+j+k;
+            }
+		} 
+	}*/
+
+    for (int i = 0; i < N; i++) {
+		for (int j = 0; j <N; j++) {
+			for (int k = 0;k < N ;k++ ){
+                //z[i][j] += 10;
+			    cout << z[i][j][k] << endl;
+            }
+		} 
+	}
+}
+
+
+void updatehe(int n, int ic, int jc, int kc,float sten1[][N][N], int occ[N ][N][N]) {
+	//sten1[i]
+	if(ic-1 !=0)
+		sten1[ic - 1][jc][kc] += 2 * occ[ic][jc][kc];
+	if(ic+1 !=N-1)
+		sten1[ic + 1][jc][kc] += 2 * occ[ic][jc][kc];
+	if(jc-1 != 0)
+		sten1[ic][jc - 1][kc] += 2 * occ[ic][jc][kc];
+	if(jc+1 != N-1)
+		sten1[ic][jc + 1][kc] += 2 * occ[ic][jc][kc];
+    if(kc+1 != N-1)
+		sten1[ic][jc][kc+1] += 2 * occ[ic][jc][kc];
+    if(kc-1 !=0)
+		sten1[ic][jc][kc-1] += 2 * occ[ic][jc][kc];        
+	//printmat(sten1);
+	//cout << endl;
+
+}
+void hamiltonian(int n, int occ[N][N][N],float phi[N][N][N],float &toten ,float &totenst) {
 	float toten1;
 	toten = 0;
 	for (int i = 1; i < n-1 ; i++) {
 		for (int j = 1; j < n-1 ; j++) {
-			toten = toten - phi[i][j] * occ[i][j];
+			for(int k = 1;k < n-1;k++){
+                toten = toten - phi[i][j][k] * occ[i][j][k];
+            }
 		}
 	}
 	toten1 = 0;
 	for (int ix =1 ; ix < n-1 ; ix++) {
 		for (int iy = 1; iy < n-1 ; iy++) {
-			toten1 = toten1 - occ[ix][iy] * (occ[(ix) + 1][iy] + occ[(ix) - 1][iy] + occ[ix][(iy) + 1] + occ[ix][(iy) - 1]);
+            for(int iz = 1;iz < n-1;iz ++){
+			    toten1 = toten1 - occ[ix][iy][iz] * (occ[(ix) + 1][iy][iz] + occ[(ix) - 1][iy] [iz]+ occ[ix][(iy) + 1][iz] + occ[ix][(iy) - 1][iz] + occ[ix][iy][iz+1]+ occ[ix][iy][iz-1]);
+                
+            }
 		}
 	}
-	toten = toten + toten1 / 2;
-	totenst = toten / n*n;
-	//cout << "toten" << toten << endl;
-
-}
-
-
-///  Stores site energy which corresponds to 4 neighbours in    sten1[][]
-void siteEnergy(int n, int occ[N][N], float phi[N][N], float (*sten1)[N]) {
-	for (int ix = 1; ix < n - 1; ix++) {
-		for (int iy = 1; iy < n - 1; iy++) {
-			for(int iz = 1;iz < n-;iz++){
-				sten1[ix][iy][iz] = -1 * phi[ix][iy];
-				sten1[ix][iy] = sten1[ix][iy] - (occ[(ix) + 1][iy] + occ[(ix) - 1][iy] + occ[ix][(iy) - 1] + occ[ix][(iy) + 1]);
-			}
-			/*cout << sten1[ix][iy] << endl;
-			cout << phi[ix][iy] << endl;*/
-		}
-	}
-	/*cout << "occ matt" << endl;
-	printmat1(occ);
-	cout << "sten mat" << endl;
-	printmat(sten1);
-	cout << endl;*/
-	
-	
-}
-
-void updatehe(int n, int ic, int jc, float (*sten1)[N], int occ[N ][N]) {
-	//sten1[i]
-	if(ic-1 !=0)
-		sten1[ic - 1][jc] += 2 * occ[ic][jc];
-	if(ic+1 !=N-1)
-		sten1[ic + 1][jc] += 2 * occ[ic][jc];
-	if(jc-1 != 0)
-		sten1[ic][jc - 1] += 2 * occ[ic][jc];
-	if(jc+1 != N-1)
-		sten1[ic][jc + 1] += 2 * occ[ic][jc];
-	//printmat(sten1);
-	//cout << endl;
+	toten = toten + toten1 / 3;
+	totenst = toten / pow(n,NDIM);
+	cout << "toten" << totenst << endl;
 
 }
 
 int main1(void) {
 
 
-	int iseed, x1, ix, ic, iy, jc, imeas, iskip, nskip, nmcs, k, nmeas;
+	int iseed, x1, ix, ic, iy, jc, kc,imeas, iskip, nskip, nmcs, k, nmeas;
 	int it, itemp, ntime, occ[N ][N ][N] = { 0 }, kconfig, nmeass[NTEMP];
 	float phi[N][N][N] = { 0 }, aaa, tempp[NTEMP] = { 0 }, phisum;
 	float toten, totenst, beta, a, c, delen, ran2, mag, temp;
@@ -195,7 +216,7 @@ int main1(void) {
 	float w = 0.0;
 	iseed = 5;
 	int mag2 = 0;
-	nskip = 1000;
+	nskip = 1;
 	for (itemp = 0; itemp < NTEMP; itemp++) {
 		tempp[itemp] =3.5 - itemp*0.5;
 		nmeass[itemp] = NMEAS;
@@ -207,34 +228,18 @@ int main1(void) {
 		cout << "config " << kconfig << endl;
 		rsiteen(N, iseed, phi, w); 
 		cout << w << endl;
-		/*for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				
-				f2 << i <<"\t" << j << "\t" <<phi[i][j] << endl;
-			}
-		}*/
+		
 		f2 << endl;
 		phisum = 0;
-		/*for (int i = 0; i < N ; i++) {
-			for (int j = 0; j < N ; j++) {
-				phisum += phi[i][j];
-			}
-		}*/
+		
 
 		rocc(N, iseed, occ);
-		/*for (int i = 0; i < N ; i++) {
-			for (int j = 0; j < N ; j++) {
-				f3 << i << "\t" << j << "\t" << occ[i][j] << endl;
-
-			}
-		}*/
+		
 		f3 << endl;
 		siteEnergy(N, occ, phi, sten1);
 		hamiltonian(N, occ, phi, toten, totenst);
-		//cout << toten << "main" << endl;
 		f4 << "initialen-toten" << "\t" << toten << "\t" <<"encrystal=" << "\t" << 2 * N*N << "\t" << phisum << "\t"  <<"endisordercrystal=" <<"\t" << - 2 * N*N-phisum  <<"\t"<<   -2 * N*N + phisum << endl;
 		f4 << endl;
-		//printmat1(occ);
 		cout << endl;
 		for (itemp = 0; itemp < NTEMP; itemp++) {
 			cout << "temp : " << tempp[itemp] << endl;
@@ -248,28 +253,24 @@ int main1(void) {
 
 				//cout <<  "  itemp : " << itemp << "  imeas :" << imeas << endl;
 				cout << "imeas :" << imeas << endl;
-				//printmat1(occ);
-				cout << endl;
-				//printmat(sten1);
+			
+				//cout << endl;
+			
 				cout  << "toten" << "\t" << toten  << endl;
-				//cout << "phisum" << "\t" << phisum << endl;
+
 				for (iskip = 0; iskip < nskip; iskip++) {
-					//printmat1(occ);
-					//cout << "iskip : " << iskip << endl;
-					//cout << "temp : " << nmeass[itemp] << "  itemp : " << itemp  << "  imeas :" << imeas << "  iskip : " << iskip <<  endl;
-					for (nmcs = 0; nmcs < NS; nmcs++) {
-						//printmat1(occ);
+						for (nmcs = 0; nmcs < NS; nmcs++) {
+				
 						ic = int(myrand(iseed)*(N-1));
 						jc = int(myrand(iseed)*(N-1));
 						kc = int(myrand(iseed)*(N-1));
 						if ((ic != 0) && (ic != N-1) && (jc != 0) && (jc != N-1) && (kc != N-1) && (kc != 0)) {
-							//delst = -2 * occ[ic][jc] * sten1[ic][jc];
-							//delst = -2 * occ[ic][jc] ;
-							delst =  2 * occ[ic][jc] * (occ[ic + 1][jc] + occ[ic - 1][jc] + occ[ic][jc + 1] + occ[ic][jc - 1]) - 2*phi[ic][jc]*occ[ic][jc];
+							
+							delst =  2 * occ[ic][jc][kc] * (occ[ic + 1][jc][kc] + occ[ic - 1][jc][kc] + occ[ic][jc + 1][kc] + occ[ic][jc - 1][kc]) - 2*phi[ic][jc][kc]*occ[ic][jc][kc];
 							if (delst <= 0) {
 								toten += delst;
-								occ[ic][jc] = -1 * occ[ic][jc];
-								updatehe(N, ic, jc, sten1, occ);
+								occ[ic][jc][kc] = -1 * occ[ic][jc][kc];
+								updatehe(N, ic, jc,kc, sten1, occ);
 								flip++;
 
 							}
@@ -278,8 +279,8 @@ int main1(void) {
 								c = myrand(iseed);
 								if (c <= a) {
 								toten += delst;
-								occ[ic][jc] = -1*occ[ic][jc];
-								updatehe(N, ic, jc, sten1, occ);
+								occ[ic][jc][kc] = -1*occ[ic][jc][kc];
+								updatehe(N, ic, jc,kc, sten1, occ);
 								flip++;
 
 								}
@@ -291,12 +292,13 @@ int main1(void) {
 				mag = 0;
 				for (int i = 0; i < N ; i++) {
 					for (int j = 0; j < N ; j++) {
-						mag += occ[i][j];
+						for(int k = 0;k < N;k++){
+                            mag += occ[i][j][k];
+                        }
 					}
 				}
 				
 				cout << mag << "\t" <<  "mag" << endl;
-				cout << mag2 << "\t" << "del(m)" << endl;
 				mag2 = mag;
 				
 				f400.open("mylog/40" + to_string(kconfig) + ".txt", ofstream::app);
@@ -305,17 +307,19 @@ int main1(void) {
 				f400 << endl;
 				f400.close();
 			}
-			//cluster call
-			//clustering(occ);
+			
 			mag = 0;
 			for (int i = 0; i < N ; i++) {
 				for (int j = 0; j < N; j++) {
-					mag += occ[i][j];
+					for(int k = 0; k < N;k++){
+                        mag += occ[i][j][k];
+                    }
 				}
 			}
 			//cout << mag << endl;
 			f4 << "kconfig" << "\t" << "temp" << "\t" << "\t" << "mag / NS" << "\t" << "toten" << endl;
 			f4 << kconfig << "\t" << temp << "\t" << "\t" << mag / NS << "\t" << toten << endl;
+            cout << mag << " -------------------final  " <<  endl;
 		}
 		//int clusters = hoshen_kopelman(matrix, N,N);
 		
@@ -323,14 +327,16 @@ int main1(void) {
 		f300.open("mylog/30" + to_string(kconfig) + ".txt", ofstream::out);
 		for (int i = 0; i < N ; i++) {
 			for (int j = 0; j < N ; j++) {
-				if (occ[i][j] < 0) {
+				for(int k = 0;k < N;k++){
+                    if (occ[i][j][k] < 0) {
 					
-					f200 << i << "\t" << j << "\t" << occ[i][j] << endl;
-				}
-				else {
+					    f200 << i << "\t" << j << "\t" << k << "\t" << occ[i][j][k] << endl;
+				    }
+				    else {
 					
-					f300 << i << "\t" << j << "\t" << occ[i][j] << endl;
-				}
+					    f300 << i << "\t" << j << "\t"  << k << "\t" << occ[i][j][k] << endl;
+				    }
+                }
 			}
 		}
 		f200.close();
@@ -344,25 +350,28 @@ int main1(void) {
 	f2.close();
 	f3.close();
 	//printmat(phi);
-	cout << flip << "\t" << "flips" << endl;
-	//printmat(sten1);
-	//printmat1(occ);
+	//cout << flip << "\t" << "flips" << endl;
+	
 	return 0;
 
 }
-void printz(float(*z)[N]) {
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 2; j++) {
-			//z[i][j] += 10;
-			cout << z[i][j] << endl;
-		} 
-	}
-}
-void main(void) {
+int main(void) {
 	
 	int a[N][N][N] = {0};
-	rocc(N,5,a);
-	
+    float f[N][N][N] = {0};
+    main1();
+	//rocc(N,5,a);
+   
+	//printz(a);
 	//main1();
-	system("pause");
+	//system("pause");
+
+    /*vector <vector <int>> ;
+    for (int )
+    v.push_back(5);
+    vector <int> ::iterator i;
+    cout << v[0] << endl;*/
+    
+    return 0;
+
 }
